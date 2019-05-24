01a35b1ed815b87b590deecde95c71eef5da912a
Cheng Wu <melodywu@Chengs-MBP.earthlink.net>
2019-05-24 19:52:56 -0400

    Apply bootstrap grayscale theme and set up top nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/'
-  # root to: 'page#index'
+  # mount Spree::Core::Engine, at: '/'
+  root to: 'page#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
