bed583da00eb09a077ac3fae7bc3614983792e03
Cheng Wu <melodywu@Chengs-MBP.earthlink.net>
2019-06-08 22:11:21 -0400

    Move the cart link to top nav bar with new cart icon and make the main nav bar responsive to the screen size

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -13,3 +13,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
